25b9fdea4cdbfb54e543efcdaa75962d7561424e
11reed <reed11tim@gmail.com>
2021-09-18 18:20:09 -0400

    changes

---
+++ README.md
@@ -0,0 +1,7 @@
+# coin-scraper
+Scrapes data from CoinGecko
+
+# Future
+Convert response to JSON
+
+Convert JSON to CSV
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use serde::{Serialize, Deserialize};
 use futures::{future, FutureExt};
 use nipper::Document;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    origin: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let res = reqwest::get("https://www.coingecko.com/en/coins/solana/historical_data/usd#panel").await?;
+
+    // assert_eq!(res.url().as_str(), "hhttps://www.coingecko.com/get");
 
     println!("Status: {}", res.status());
 
@@ -11,23 +19,23 @@ async fn main() -> Result<(), reqwest::Error> {
 
     // println!("Body:\n\n{}", body);
 
-    println!("Solana");
-
     let document = Document::from(res.as_str());
+
+    println!("Solana Historical Data");
 
     document.select("tbody").iter().for_each(|coin_data| {
         let info = coin_data.select("tr");
         println!("{}", info.text());
     });
 
-    let futures = vec![btc_data().boxed(), eth_data().boxed()];
+    let futures = vec![sol_info().boxed()];
     let _results = future::join_all(futures).await;
 
     Ok(())
 }
 
-async fn btc_data() -> Result<(), reqwest::Error> {
-    let res = reqwest::get("https://www.coingecko.com/en/coins/solana/historical_data/usd#panel").await?;
+async fn sol_info() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://www.coingecko.com/en/coins/solana/news").await?;
 
     println!("Status: {}", res.status());
 
@@ -35,31 +43,13 @@ async fn btc_data() -> Result<(), reqwest::Error> {
 
     let document = Document::from(res.as_str());
 
-    println!("Bitcoin");
+    println!("Solana News and Links");
 
-    document.select("tbody").iter().for_each(|coin_data| {
-        let info = coin_data.select("tr");
+    document.select("article").iter().for_each(|coin_data| {
+        let info = coin_data.select("h2");
+        let href = coin_data.select("a");
         println!("{}", info.text());
-        println!();
-    });
-
-    Ok(())
-}
-
-async fn eth_data() -> Result<(), reqwest::Error> {
-    let res = reqwest::get("https://www.coingecko.com/en/coins/solana/historical_data/usd#panel").await?;
-
-    println!("Status: {}", res.status());
-
-    let res = res.text().await?;
-
-    let document = Document::from(res.as_str());
-
-    println!("Ethereum");
-
-    document.select("tbody").iter().for_each(|coin_data| {
-        let info = coin_data.select("tr");
-        println!("{}", info.text());
+        println!("{}", href.attr("href").unwrap());
         println!();
     });
 
